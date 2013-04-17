f82041a83bcce2ddc4d01a0d868efb1ff63b3fad
Dylan <dylan@proggy.nl>
2013-04-17 16:05:21 +0200

    Fixed scaling issue for all drawables. Added getters and setters for scale and position so the rectangle can be updated. Added currentRectangle method.

--- Beasties/src/nl/hr/student/ddophemont/beasties/Drawable.java
+++ Beasties/src/nl/hr/student/ddophemont/beasties/Drawable.java
@@ -13,22 +13,27 @@ public abstract class Drawable implements IDrawable {
 	
 	private DrawArea _drawArea;
 	private TextureRegion _texReg;
-
-	public Vector2 pos;
-	public float scale;
+	private Rectangle _rectangle;
+	private float _scale;
+	public Vector2 _pos;
+	
 	public float rotation;
 	
 	public Drawable( Vector2 position, DrawArea drawArea ) {
 		if ( _texture == null ) {
+			// TODO: create some kind of exception or error if the texture is not set
 			// "Please set the texture first before creating childs of the Drawable class. Use the static method Drawable.setTexture(Texture texture) to solve this issue." 
 		}
 		
 		_drawArea = drawArea;
 		_texReg = new TextureRegion( _texture, _drawArea.x(), _drawArea.y(), _drawArea.width(), _drawArea.height() );
-		pos = position;
-		scale = DRAWABLE_SCALE;
+		_pos = position;
+		_scale = DRAWABLE_SCALE;
 		rotation = 0;
+		
+		_rectangle = new Rectangle( _pos.x - _drawArea.width()/2*DRAWABLE_SCALE, _pos.y - _drawArea.height()/2*DRAWABLE_SCALE, _drawArea.width()*DRAWABLE_SCALE, _drawArea.height()*DRAWABLE_SCALE );
 	}
+
 
 	public static void setTexture(Texture texture) {
 		_texture = texture;
@@ -38,20 +43,45 @@ public abstract class Drawable implements IDrawable {
 	public void draw(float delta) {
 		Beasties.spriteBatch.draw(
 			_texReg,
-			pos.x,
-			pos.y,
-			_drawArea.width()/2,
-			_drawArea.height()/2,
+			_pos.x,
+			_pos.y,
+			_drawArea.width()/2*DRAWABLE_SCALE,
+			_drawArea.height()/2*DRAWABLE_SCALE,
 			_drawArea.width(),
 			_drawArea.height(),
-			scale,
-			scale,
+			_scale,
+			_scale,
 			rotation
 		);
 	}
 	
-	public Rectangle collisionRectangle() {
-		return new Rectangle( _drawArea.x(), _drawArea.y(), _drawArea.width(), _drawArea.height() );
+	public Rectangle currentRectangle() {
+		return _rectangle;
+	}
+	
+	private void _updateRectangle() {
+		_rectangle.x = _pos.x - _drawArea.width()/_scale*DRAWABLE_SCALE;
+		_rectangle.y = _pos.y - _drawArea.height()/_scale*DRAWABLE_SCALE;
+		_rectangle.width = _drawArea.width()*_scale*DRAWABLE_SCALE;
+		_rectangle.height = _drawArea.height()*_scale*DRAWABLE_SCALE;
+	}
+
+	public float getScale() {
+		return _scale;
+	}
+
+	public void setScale( float scale ) {
+		_scale = _scale;
+		_updateRectangle();
+	}
+	
+	public Vector2 getPos() {
+		return _pos;
+	}
+
+	public void setPos( Vector2 position ) {
+		_pos = position;
+		_updateRectangle();
 	}
 	
 }
