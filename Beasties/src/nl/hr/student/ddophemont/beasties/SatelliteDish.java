9d9124e666ab183c1fc26b90c25d7c3594019ec7
Dylan <dylan@proggy.nl>
2013-04-23 10:16:20 +0200

    Added the players satellite dish.

---
+++ Beasties/src/nl/hr/student/ddophemont/beasties/SatelliteDish.java
@@ -0,0 +1,11 @@
+package nl.hr.student.ddophemont.beasties;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class SatelliteDish extends GameObject {
+
+	public SatelliteDish( Vector2 position ) {
+		super(position, DrawArea.Base() );
+	}
+
+}
