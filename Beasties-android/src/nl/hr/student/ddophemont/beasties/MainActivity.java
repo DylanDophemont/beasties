18c7495f0e2a824d7c5489bb617889c19e3a0c4a
Dylan <dylan@proggy.nl>
2013-04-24 11:52:03 +0200

    Status bar is hidden on android

--- Beasties-android/src/nl/hr/student/ddophemont/beasties/MainActivity.java
+++ Beasties-android/src/nl/hr/student/ddophemont/beasties/MainActivity.java
@@ -1,6 +1,8 @@
 package nl.hr.student.ddophemont.beasties;
 
+import nl.hr.student.ddophemont.beasties.enemies.Beasties;
 import android.os.Bundle;
+import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -15,4 +17,14 @@ public class MainActivity extends AndroidApplication {
         
         initialize(new Beasties(), cfg);
     }
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onResume() {
+		View rootView = getWindow().getDecorView();
+		rootView.setSystemUiVisibility(View.STATUS_BAR_HIDDEN);
+		
+		super.onResume();
+	}
+    
 }
